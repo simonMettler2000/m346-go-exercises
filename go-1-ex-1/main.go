df96a70d5ee22ae423c2a28a2c8bb06aa54a73f9
Patrick Bucher <patrick.bucher@sluz.ch>
2024-11-21 16:11:16 +0100

    Revert "exercise 1"
    This reverts commit 131562d030cc3efdc6a6768438bf29462f602d25.

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,18 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var firstName string = "Patrick"
-	lastName := "Bucher"
-
-	var dayOfBirth byte = 24
-	monthOfBirth := 6
-	yearOfBirth := 1923
-
-	var numberOfSiblings byte = 1
-	var heightInMeters float32 = 1.88
-
-	zodiacSign := '\u264b'
-
+	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
