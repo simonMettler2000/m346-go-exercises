b46d834a064785d2d594c47930e69683bf2c7421
Artur Ferreira <artur.ferreira@ckw.ch>
2024-11-22 14:39:16 +0100

    declared variables

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,12 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName = "Artur"
+	var lastName = "Ferreira"
+	var dayOfBirth = "12.09.2007"
+	var numberOfSiblings = 2
+	var heightInMeters = 1.70
+	var zodiacSign = 'virgo'
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
