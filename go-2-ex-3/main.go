dfd681107eb205c1d2659e41cd1e9ec381b3345e
Artur Ferreira <artur.fer.cruz@gmail.com>
2024-11-22 15:58:32 +0100

    2 ex 3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,11 +4,18 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
-
+	modules := map[uint16]string{
+		104: "Ka",
+		117: "VonAllmen",
+		346: "Cloud",
+	}
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
+	delete(modules, 117)
+	modules[114] = "Codierung"
+	modules[346] = "Cloud computing"
 	// TODO: delete one
 	// TODO: add one
 	// TODO: replace one
