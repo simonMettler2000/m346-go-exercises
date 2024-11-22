0ba15da9c02f45a89f2567fdfd7515fd84798211
simon mettler <lamasion560gmail.com>
2024-11-22 15:38:20 +0100

    commit hi

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,35 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   byte
+	Month byte
+	Year  int
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	FullName         FullName
+	BirthDate        BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName:         FullName{FirstName: "Simon", LastName: "Mettler"},
+		NumberOfSiblings: 2,   // TODO: adjust
+		ZodiacSign:       '🗿', // TODO: adjust
+		BirthDate:        BirthDate{Day: 11, Month: 2, Year: 2000},
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var fibs = []int{1, 1, 0, 0, 0}
+	var fibs = []int{1, 1, 2, 3, 5}
 
 	fibs[2] = fibs[0] + fibs[1]
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, 8) // TODO: replace 0 with the next Fibonacci number
 	// TODO: compute three more Fibonacci numbers and append them
-
+	fibs = append(fibs, 13)
+	fibs = append(fibs, 21)
+	fibs = append(fibs, 34)
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
