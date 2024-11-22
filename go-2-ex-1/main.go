f64575ead156314c5463d85bfeb0e77894ef1b01
Artur Ferreira <artur.fer.cruz@gmail.com>
2024-11-22 15:34:56 +0100

    Add FullName and BirthDate structures to Profile and initialize in main

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,20 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
-
+type BirthDate struct {
+	Day   byte
+	Month byte
+	Year  int
+}
 type Profile struct {
 	// TODO: embed full name and birth date information
+	FullName         FullName
+	BirthDate        BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +25,16 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		FullName:         FullName{FirstName: "Artur", LastName: "Ferreira"},
+		NumberOfSiblings: 2,   // TODO: adjust
+		ZodiacSign:       '♍', // TODO: adjust
+		BirthDate:        BirthDate{Day: 12, Month: 9, Year: 2007},
 	}
+
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
