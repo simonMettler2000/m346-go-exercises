1b9ebbf9a817003f813160ac3d96c564bc61ffab
Artur Ferreira <artur.fer.cruz@gmail.com>
2024-11-29 09:22:51 +0100

    Implement Student and Class types with sample data and output

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,12 +1,47 @@
 package main
 
+import "fmt"
+
+type Student struct {
+    FirstName string
+    LastName  string
+}
+
+type Class struct {
+    Name     string
+    Students []Student
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	struct Student { 
-		firstName string
-		lastName string
-	}
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+    INA23aL := Class{
+        Name: "INA23aL",
+        Students: []Student{
+            {"Simon", "Mettler"},
+            {"Gian-Marco", "Wismer"},
+            {"Artur", "Ferreira"},
+        },
+    }
+
+    INA23bL := Class{
+        Name: "INA23bL",
+        Students: []Student{
+            {"Marco", "Wismer"},
+            {"Nicolas", "Rapedius"},
+            {"Yves", "Troxler"},
+        },
+    }
+
+    modules := map[int][]Class{
+        114: {INA23aL, INA23bL},
+        254: {INA23aL},
+        320: {INA23bL},
+    }
+
+    fmt.Println("Classes:")
+    fmt.Println("INF20:", INA23aL)
+    fmt.Println("INF21:", INA23bL)
+    fmt.Println("\nModules:")
+    fmt.Println("Module 346:", modules[346])
+    fmt.Println("Module 254:", modules[254])
+    fmt.Println("Module 320:", modules[320])
 }
