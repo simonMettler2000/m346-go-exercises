d8a4d21a90d5acefbfc98ca09066aefc7700e545
Artur Ferreira <artur.fer.cruz@gmail.com>
2024-11-29 08:53:25 +0100

    Add Student struct with first and last name fields

--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -2,6 +2,10 @@ package main
 
 func main() {
 	// TODO: declare a type for Student (with first and last name)
+	struct Student { 
+		firstName string
+		lastName string
+	}
 	// TODO: declare a type for Class (consisting of multiple students)
 	// TODO: declare a map of modules being attended by multiple classes
 	// TODO: output everything using fmt.Println()
