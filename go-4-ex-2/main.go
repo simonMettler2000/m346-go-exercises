e68c849e1517e9f3a87906ef008ff0d138d2ae31
Simon Mettler <simon.mettler@kauz.ch>
2024-12-13 13:08:53 +0100

    Ex-4-2 done

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,15 @@
 package main
-
+import "fmt"
+import "math"
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypotenuse() float64{
+	var a float64 = 7
+	var b float64 = 4
+	c := math.Sqrt(math.Pow(a,2)+math.Pow(b,2))
+	return c
+}
 
 func main() {
 	// TODO: call the function computeHypotenuse
+	fmt.Print(computeHypotenuse())
 }
